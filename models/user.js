39f21302b6cf7b2ce696fbddf9e3828b509861cb
Light <kiaraymg@gmail.com>
2020-06-05 11:25:36 +0200

    Installed mongoose.

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    date: { type: Date, required: true },
+
+});
+
+module.exports = mongoose.model('User', userSchema);
